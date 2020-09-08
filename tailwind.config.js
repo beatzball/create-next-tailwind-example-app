9403204d6a2032b9442e16f9082bf269b167de39
beatzball <beatzball@imzaid.com>
2020-09-07 23:55:56 -0700

    update(tailwind): remove fullhuman, apply tw config purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 const { colors } = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: true,
+    content: [
+      './components/**/*.tsx',
+      './pages/**/*.tsx',
+      './components/**/*.js',
+      './pages/**/*.js',
+    ],
+  },
   theme: {
     fontFamily: {},
     extend: {
@@ -10,17 +23,17 @@ module.exports = {
         neutral: colors.gray,
       },
       fontSize: {
-        '12': '12px',
-        '14': '14px',
-        '16': '16px',
-        '18': '18px',
-        '20': '20px',
-        '24': '24px',
-        '30': '30px',
-        '36': '36px',
-        '48': '48px',
-        '60': '60px',
-        '80': '80px',
+        12: '12px',
+        14: '14px',
+        16: '16px',
+        18: '18px',
+        20: '20px',
+        24: '24px',
+        30: '30px',
+        36: '36px',
+        48: '48px',
+        60: '60px',
+        80: '80px',
       },
     },
   },
