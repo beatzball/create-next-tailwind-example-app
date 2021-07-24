8c58db1fe89604267c625fb07318dfcab1b8f9bc
beatzball <beatzball@imzaid.com>
2021-07-23 17:21:49 -0700

    chore: moved app sourcecode under src/*

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   mode: 'jit',
   darkMode: 'class',
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     fontFamily: {},
     extend: {
